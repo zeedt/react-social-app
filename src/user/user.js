61b67cd1b35d5503f531f877cc75e6779157f695
Saheed Yusuf <yusufsaheedtaiwo@yahoo.com>
2020-02-10 15:37:18 +0100

    update on chat

--- src/user/user.js
+++ src/user/user.js
@@ -13,16 +13,19 @@ class Users extends Component {
         super(props);
         this.userMap = new Map();
         this.distinctUser = [];
-        props.loadUsers();
+        // props.loadUsers();
         this.state = {
             open: false,
             socketEndpointUrl: 'localhost:3001',
-            onlineUsers: []
+            onlineUsers: [],
+            currentUserChattingWith: '',
+            message: '',
+            chats: {}
         };
         this.socket = SocketIOClient(this.state.socketEndpointUrl);
 
         this.socket.on('connection-received', () => {
-            this.socket.emit('join', {username : window.localStorage.username, first_name : window.localStorage.first_name, last_name : window.localStorage.last_name});
+            this.socket.emit('join', { username: window.localStorage.username, first_name: window.localStorage.first_name, last_name: window.localStorage.last_name });
         })
 
         this.socket.on('all-users', data => {
@@ -30,14 +33,20 @@ class Users extends Component {
             this.distinctUser = [];
             const map = new Map();
             for (const item of data) {
-                console.log("iterating")
-                if (!map.has(item.username)) {
-                    console.log("Adding");
-                    map.set(item.username, true);    // set any value to Map
+                if (window.localStorage.getItem('username') !== item.username && !map.has(item.username)) {
+                    map.set(item.username, true);
                     this.distinctUser.push({
                         username: item.username,
                         name: item.name
                     });
+
+                    if (this.state.chats[this.state.currentUserChattingWith] === undefined) {
+                        console.log("Setting chat empty array for user");
+                        let newChats = Object.assign({}, this.state.chats, { [item.username]: [] });
+                        this.setState({
+                            chats: newChats
+                        });
+                    }
                 }
             }
             this.setState({
@@ -45,14 +54,56 @@ class Users extends Component {
             })
         });
 
-    }
+        this.openOrCloseChat = this.openOrCloseChat.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.socket.on('private-message-received', data => {
+            var elem = document.getElementById('bottomChatDiv');
+            elem.scrollTop = elem.scrollHeight ;
+            console.log("Private message " + JSON.stringify(data));
+            if (data.toUsername !== window.localStorage.getItem('username') || data.fromUsername !== window.localStorage.getItem('username')) {
+                const user = data.toUsername !== window.localStorage.getItem('username') ? data.toUsername : data.fromUsername;
+                var userMessage = this.state.chats[user];
+                userMessage = (userMessage === undefined) ? [] : userMessage;
+                userMessage.push(data);
+                let newChats = Object.assign({}, this.state.chats, { [user]: userMessage });
+                this.setState({
+                    chats: newChats
+                });
+                console.log("New chats " + JSON.stringify(this.state.chats));
+            } else {
+                console.log("Sender and receiver match current logged in user");
+            }
 
-    getDistinctUserFromOnlineUserSockets = () => {
-        this.state.onlineUsers.map(user => {
-            this.userMap.set(user.username, user.username);
+
         });
     }
 
+    openOrCloseChat = (username) => {
+        console.log("Opening chat for " + username);
+        this.setState({
+            currentUserChattingWith: username,
+            open: !this.state.open
+        });
+    }
+
+    sendMessage = () => {
+        if (this.state.message === undefined || this.state.message === '') { return; }
+        const messageObject = {
+            message: this.state.message,
+            fromUsername: window.localStorage.username,
+            toUsername: this.state.currentUserChattingWith
+        };
+
+        this.socket.emit('private-message-sent', messageObject);
+        this.setState({ message: '' });
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            message: e.target.value
+        });
+    }
     render() {
         return (
             <div>
@@ -62,7 +113,7 @@ class Users extends Component {
             </Card.Header>
                     <Card.Body className='user-list-card-body'>
                         {this.state.onlineUsers.map(user =>
-                            <div key={user.username} className='user-list-div' onClick={() => { this.setState({ open: !this.state.open }) }} aria-controls="example-fade-text"
+                            <div key={user.username} className='user-list-div' onClick={() => { this.openOrCloseChat(user.username) }} aria-controls="example-fade-text"
                                 aria-expanded={this.state.open}>
                                 <img src='image/logo.png' height={30} width={30} className='user-chat-image' alt='ImageAbsent' /> {user.name}
 
@@ -73,45 +124,21 @@ class Users extends Component {
                 <Collapse in={this.state.open} className='collapse-view'>
                     <div id="example-fade-text">
                         <div className="chat">
-                            <div className="title">This is the chat title</div>
-                            <div className="bottomChatDiv text">
-                                <p>Text 1</p>
-                                <p>Text 2</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
-                                <p>Text 3</p>
+                            <div className="title">{this.state.currentUserChattingWith}</div>
+                            <div className="bottomChatDiv text" id='bottomChatDiv'>
+                                {this.state.currentUserChattingWith === '' ? null : this.state.chats[this.state.currentUserChattingWith].map(chat => <span>
+                                {window.localStorage.getItem('username') === chat.fromUsername ? <p style={{backgroundColor:'#e8e8e8', float:'right', marginLeft:'35px', borderRadius:'3px', padding:'3px'}}>
+                                    {chat.message}</p> : 
+                                <p style={{backgroundColor:'#e8e8e8', float:'left', marginRight:'35px', borderRadius:'3px', padding:'3px'}}>{chat.message}</p>}
+                                </span>
+                                )}
                             </div>
                             <div className="">
                                 <div className="input-group mb-3">
 
-                                    <input type='text' name="message" className="form-control " onChange={this.handleInputChange} placeholder="Type message here..." />
+                                    <input type='text' name="message" value={this.state.message} className="form-control " onChange={this.handleInputChange} placeholder="Type message here..." />
                                     <div className="input-group-append">
-                                        <span className="input-group-text ficon" ><FontAwesomeIcon icon={faArrowRight} color='white' /></span>
+                                        <span className="input-group-text ficon" ><FontAwesomeIcon icon={faArrowRight} color='white' onClick={this.sendMessage} /></span>
                                     </div>
                                 </div>
                             </div>
@@ -139,4 +166,5 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default Users;
+// export default connect(mapStateToProps, mapDispatchToProps)(Users);
